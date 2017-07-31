fd351e51c91c74ba4b4851016830b0a85a47b9fc
T-Dex <dex.m.mills@gmail.com>
2017-07-30 18:06:54 -0600

    Updated JSON files

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var express = require ('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 3000))
+app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/build'))
+app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(request, response){
-  response.sendFile(__dirname + '/build/index.html')
-})
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.listen(app.get('port'), function(){
-  console.log("Express server started on port", appe.get('port'))
-})
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
